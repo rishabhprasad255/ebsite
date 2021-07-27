ab10159358e7b68b462677fc138335367ebd409d
rishabhprasad255 <rishabhprasad490@gmail.com>
2021-07-27 19:45:56 +0530

    fixed bugs

--- src/components/Carousell.js
+++ src/components/Carousell.js
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import style from "../css/carousel.module.css";
-import first from "../assets/first.jpg";
-import last from "../assets/last.jpg";
-import middle from "../assets/middle.jpg";
 
 function Carousell() {
   const [index, setIndex] = useState(0);
@@ -18,21 +15,33 @@ function Carousell() {
       onSelect={handleSelect}
     >
       <Carousel.Item>
-        <img className="d-block w-100" src={first} alt="First slide" />
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1483985988355-763728e1935b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+          alt="First slide"
+        />
         <Carousel.Caption>
           <h3> Styles for Men and women</h3>
           <p></p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" src={last} alt="Second slide" />
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1562183241-b937e95585b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1001&q=80"
+          alt="Second slide"
+        />
 
         <Carousel.Caption>
           <h3>Shoes</h3>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" src={middle} alt="Third slide" />
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1560243563-062bfc001d68?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80"
+          alt="Third slide"
+        />
 
         <Carousel.Caption>
           <h3>One Stop Solution For You</h3>
